c72107673b553bd8bb92f384d3cb1961388493b0
Muriithi Ian <imuriithiian@gmail.com>
2022-08-12 14:31:51 +0300

    Pass test 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Susan";
-const height = 74;
+const name = "Susan"; // pass test 1
+const height = 25; // pass test 2
 const message = `${name} is ${height} inches tall`;
 
 module.exports = { name, height, message };
